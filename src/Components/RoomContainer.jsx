239ac59a1bc88e59f651bd09184c5f40f0935224
shimehituzi <shimehituzi@gmail.com>
2019-11-14 20:15:34 +0900

    クラスコンポーネントにまとめた

--- src/Components/RoomContainer.jsx
+++ src/Components/RoomContainer.jsx
@@ -3,26 +3,20 @@ import ExpansionPanel from '@material-ui/core/ExpansionPanel'
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 
-let Room = (props) => {
-  return(
-    <div>
-      {props.roomsData.map((room) => {
-        return(
-          <ExpansionPanel key={room.id} >
-            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />} >
-              <strong>{room.name}</strong>
-            </ExpansionPanelSummary>
-          </ExpansionPanel>
-          )
-      })}
-    </div>
-  )
-}
-
 class RoomContainer extends React.Component {
   render() {
     return(
-      <Room roomsData={this.props.roomsData}/>
+      <div>
+        {this.props.roomsData.map((room) => {
+          return(
+            <ExpansionPanel key={room.id} >
+              <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />} >
+                <strong>{room.name}</strong>
+              </ExpansionPanelSummary>
+            </ExpansionPanel>
+          )
+        })}
+      </div>
     )
   }
 }
