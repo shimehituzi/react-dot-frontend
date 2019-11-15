054865560ba1493838bf30299aa8d5d7ceaec037
shimehituzi <shimehituzi@gmail.com>
2019-11-15 16:57:17 +0900

    センタリングした

--- src/Components/CommentsContainer.jsx
+++ src/Components/CommentsContainer.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import {List} from '@material-ui/core'
+import {List, Grid} from '@material-ui/core'
 import CommentContainer from './CommentContainer'
 
 let CommentsContainer = props => {
   return(
-    <List>
-      {props.commentsData.map((comment) => {
-        return(
-          <CommentContainer comment={comment} key={comment.id}/>
-        )
-      })}
-    </List>
+    <Grid container justify = "center">
+      <List>
+        {props.commentsData.map((comment) => {
+          return(
+            <CommentContainer comment={comment} key={comment.id}/>
+          )
+        })}
+      </List>
+    </Grid>
   )
 }
 
