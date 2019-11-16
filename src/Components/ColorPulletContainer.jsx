9a2f96260d6d9ff9faf152afbea967a6f2373305
shimehituzi <shimehituzi@gmail.com>
2019-11-16 14:51:24 +0900

    カラーパレット表示

--- src/Components/ColorPulletContainer.jsx
+++ src/Components/ColorPulletContainer.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup'
+import ToggleButton from '@material-ui/lab/ToggleButton'
 
 class ColorPulletContainer extends React.Component {
   render() {
     return(
-      <div></div>
+      <Grid container justify="center" style={{marginBottom: "10%"}}>
+        <ToggleButtonGroup size="small">
+          <ToggleButton style={{background: "#ff8a80", width: "30px", height: "30px"}}></ToggleButton>
+          <ToggleButton style={{background: "#82b1ff", width: "30px", height: "30px"}}></ToggleButton>
+          <ToggleButton style={{background: "#b9f6ca", width: "30px", height: "30px"}}></ToggleButton>
+          <ToggleButton style={{background: "#ffff8d", width: "30px", height: "30px"}}></ToggleButton>
+          <ToggleButton style={{background: "#666666", width: "30px", height: "30px"}}></ToggleButton>
+          <ToggleButton style={{background: "#ffffff", width: "30px", height: "30px"}}></ToggleButton>
+        </ToggleButtonGroup>
+      </Grid>
     )
   }
 }
